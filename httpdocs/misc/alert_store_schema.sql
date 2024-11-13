6cfd45b648c2e35b2f97cb5c016fd68cdf3597f4
Alfredo Cardigliano <cardigliano@ntop.org>
2024-11-13 14:42:49 +0100

    Update sqlite schema

--- httpdocs/misc/alert_store_schema.sql
+++ httpdocs/misc/alert_store_schema.sql
@@ -449,7 +449,7 @@ ATTACH DATABASE ':memory:' AS mem_db;
 -- -----------------------------------------------------
 -- Table `active_monitoring_alerts`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `active_monitoring_alerts` (
+CREATE TABLE IF NOT EXISTS `mem_db.engaged_active_monitoring_alerts` (
 `rowid` INTEGER PRIMARY KEY,
 `alert_id` INTEGER NOT NULL CHECK(`alert_id` >= 0),
 `alert_status` INTEGER NOT NULL CHECK(`alert_status` >= 0) DEFAULT 0, -- e.g., historical [0], acknowledged [1], engaged (TBD)
@@ -474,89 +474,9 @@ CREATE TABLE IF NOT EXISTS `active_monitoring_alerts` (
 @
 
 -- -----------------------------------------------------
--- Table `flow_alerts`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `flow_alerts` (
-`rowid` INTEGER PRIMARY KEY,
-`alert_id` INTEGER NOT NULL CHECK(`alert_id` >= 0),
-`alert_status` INTEGER NOT NULL CHECK(`alert_status` >= 0) DEFAULT 0,
-`interface_id` INTEGER NULL,
-`tstamp` DATETIME NOT NULL,
-`tstamp_end` DATETIME NULL DEFAULT 0,
-`severity` INTEGER NOT NULL CHECK(`severity` >= 0),
-`score` INTEGER NOT NULL DEFAULT 0 CHECK(`score` >= 0),
-`counter` INTEGER NOT NULL DEFAULT 0 CHECK(`counter` >= 0),
-`json` TEXT NULL,
-`ip_version` INTEGER NOT NULL DEFAULT 0 CHECK(`ip_version` = 4 OR `ip_version` = 6),
-`cli_ip` TEXT NOT NULL,
-`srv_ip` TEXT NOT NULL,
-`cli_port` INTEGER NOT NULL DEFAULT 0 CHECK(`cli_port` BETWEEN 0 AND 65535),
-`srv_port` INTEGER NOT NULL DEFAULT 0 CHECK(`srv_port` BETWEEN 0 AND 65535),
-`vlan_id` INTEGER NOT NULL DEFAULT 0 CHECK(`vlan_id` >= 0),
-`is_cli_attacker` INTEGER NOT NULL DEFAULT 0 CHECK(`is_cli_attacker` IN (0,1)),
-`is_cli_victim` INTEGER NOT NULL DEFAULT 0 CHECK(`is_cli_victim` IN (0,1)),
-`is_srv_attacker` INTEGER NOT NULL DEFAULT 0 CHECK(`is_srv_attacker` IN (0,1)),
-`is_srv_victim` INTEGER NOT NULL DEFAULT 0 CHECK(`is_srv_victim` IN (0,1)),
-`proto` INTEGER NOT NULL DEFAULT 0 CHECK(`proto` >= 0),
-`l7_proto` INTEGER NOT NULL DEFAULT 0 CHECK(`l7_proto` >= 0),
-`l7_master_proto` INTEGER NOT NULL DEFAULT 0 CHECK(`l7_master_proto` >= 0),
-`l7_cat` INTEGER NOT NULL DEFAULT 0 CHECK(`l7_cat` >= 0),
-`cli_name` TEXT NULL,
-`srv_name` TEXT NULL,
-`cli_country` TEXT NULL,
-`srv_country` TEXT NULL,
-`cli_blacklisted` INTEGER NOT NULL DEFAULT 0 CHECK(`cli_blacklisted` IN (0,1)),
-`srv_blacklisted` INTEGER NOT NULL DEFAULT 0 CHECK(`srv_blacklisted` IN (0,1)),
-`cli2srv_bytes` INTEGER NOT NULL DEFAULT 0 CHECK(`cli2srv_bytes` >= 0),
-`srv2cli_bytes` INTEGER NOT NULL DEFAULT 0 CHECK(`srv2cli_bytes` >= 0),
-`cli2srv_pkts` INTEGER NOT NULL DEFAULT 0 CHECK(`cli2srv_pkts` >= 0),
-`srv2cli_pkts` INTEGER NOT NULL DEFAULT 0 CHECK(`srv2cli_pkts` >= 0),
-`first_seen` DATETIME NOT NULL DEFAULT 0,
-`community_id` TEXT NULL,
-`alerts_map` BLOB DEFAULT 0, -- An HEX bitmap of all flow statuses
-`flow_risk_bitmap` INTEGER NOT NULL DEFAULT 0,
-`user_label` TEXT NULL,
-`user_label_tstamp` DATETIME NULL DEFAULT 0,
-`require_attention` INTEGER NULL DEFAULT 0
-);
-
-@
-
--- -----------------------------------------------------
--- Table `host_alerts`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `host_alerts` (
-`rowid` INTEGER PRIMARY KEY,
-`alert_id` INTEGER NOT NULL CHECK(`alert_id` >= 0),
-`alert_status` INTEGER NOT NULL CHECK(`alert_status` >= 0) DEFAULT 0,
-`interface_id` INTEGER NULL,
-`ip_version` INTEGER NOT NULL DEFAULT 0 CHECK(`ip_version` = 4 OR `ip_version` = 6),
-`ip` TEXT NOT NULL,
-`vlan_id` INTEGER NULL DEFAULT 0 CHECK(`vlan_id` >= 0),
-`name` TEXT NULL,
-`is_attacker` INTEGER NULL CHECK(`is_attacker` IN (0,1)),
-`is_victim` INTEGER NULL CHECK(`is_victim` IN (0,1)),
-`is_client` INTEGER NULL CHECK(`is_client` IN (0,1)),
-`is_server` INTEGER NULL CHECK(`is_server` IN (0,1)),
-`tstamp` DATETIME NOT NULL,
-`tstamp_end` DATETIME NULL DEFAULT 0,
-`severity` INTEGER NOT NULL CHECK(`severity` >= 0),
-`score` INTEGER NOT NULL DEFAULT 0 CHECK(`score` >= 0),
-`granularity` INTEGER NOT NULL DEFAULT 0 CHECK(`granularity` >= 0),
-`counter` INTEGER NOT NULL DEFAULT 0 CHECK(`counter` >= 0),
-`description` TEXT NULL,
-`json` TEXT NULL,
-`user_label` TEXT NULL,
-`user_label_tstamp` DATETIME NULL DEFAULT 0,
-`require_attention` INTEGER NULL DEFAULT 0
-);
-
-@
-
--- -----------------------------------------------------
 -- Table `mac_alerts`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mac_alerts` (
+CREATE TABLE IF NOT EXISTS `mem_db.engaged_mac_alerts` (
 `rowid` INTEGER PRIMARY KEY,
 `alert_id` INTEGER NOT NULL CHECK(`alert_id` >= 0),
 `alert_status` INTEGER NOT NULL CHECK(`alert_status` >= 0) DEFAULT 0,
@@ -584,7 +504,7 @@ CREATE TABLE IF NOT EXISTS `mac_alerts` (
 -- -----------------------------------------------------
 -- Table `snmp_alerts`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `snmp_alerts` (
+CREATE TABLE IF NOT EXISTS `mem_db.engaged_snmp_alerts` (
 `rowid` INTEGER PRIMARY KEY,
 `alert_id` INTEGER NOT NULL CHECK(`alert_id` >= 0),
 `alert_status` INTEGER NOT NULL CHECK(`alert_status` >= 0) DEFAULT 0,
@@ -611,7 +531,7 @@ CREATE TABLE IF NOT EXISTS `snmp_alerts` (
 -- -----------------------------------------------------
 -- Table `network_alerts`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `network_alerts` (
+CREATE TABLE IF NOT EXISTS `mem_db.engaged_network_alerts` (
 `rowid` INTEGER PRIMARY KEY ,
 `local_network_id` INTEGER NOT NULL CHECK(`local_network_id` >= 0),
 `alert_id` INTEGER NOT NULL CHECK(`alert_id` >= 0),
@@ -637,7 +557,7 @@ CREATE TABLE IF NOT EXISTS `network_alerts` (
 -- -----------------------------------------------------
 -- Table `interface_alerts`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `interface_alerts` (
+CREATE TABLE IF NOT EXISTS `mem_db.engaged_interface_alerts` (
 `rowid` INTEGER PRIMARY KEY,
 `ifid` INTEGER NOT NULL CHECK(`ifid` >= -1),
 `alert_id` INTEGER NOT NULL CHECK(`alert_id` >= 0),
@@ -664,7 +584,7 @@ CREATE TABLE IF NOT EXISTS `interface_alerts` (
 -- -----------------------------------------------------
 -- Table `user_alerts`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `user_alerts` (
+CREATE TABLE IF NOT EXISTS `mem_db.engaged_user_alerts` (
 `rowid` INTEGER PRIMARY KEY,
 `alert_id` INTEGER NOT NULL CHECK(`alert_id` >= 0),
 `alert_status` INTEGER NOT NULL CHECK(`alert_status` >= 0) DEFAULT 0,
@@ -688,7 +608,7 @@ CREATE TABLE IF NOT EXISTS `user_alerts` (
 -- -----------------------------------------------------
 -- Table `system_alerts`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `system_alerts` (
+CREATE TABLE IF NOT EXISTS `mem_db.engaged_system_alerts` (
 `rowid` INTEGER PRIMARY KEY,
 `alert_id` INTEGER NOT NULL CHECK(`alert_id` >= 0),
 `alert_status` INTEGER NOT NULL CHECK(`alert_status` >= 0) DEFAULT 0,
@@ -706,10 +626,6 @@ CREATE TABLE IF NOT EXISTS `system_alerts` (
 `user_label_tstamp` DATETIME NULL DEFAULT 0,
 `require_attention` INTEGER NULL DEFAULT 0
 );
-
-
-
-
 
 @
 
@@ -751,6 +667,7 @@ require_attention INTEGER NULL DEFAULT 0
 -- -----------------------------------------------------
 
 @
+
 CREATE TEMP VIEW active_monitoring_alerts_view AS
 SELECT * FROM active_monitoring_alerts
 UNION ALL
