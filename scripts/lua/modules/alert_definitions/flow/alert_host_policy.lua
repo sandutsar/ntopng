133f5339b3d80fd900ee2d413460403b782ca335
Matteo Biscosi <biscosi@ntop.org>
2024-12-19 10:23:46 +0100

    Added host policy alert in lua

---
+++ scripts/lua/modules/alert_definitions/flow/alert_host_policy.lua
@@ -0,0 +1,52 @@
+--
+-- (C) 2019-24 - ntop.org
+--
+
+-- ##############################################
+
+local flow_alert_keys = require "flow_alert_keys"
+-- Import the classes library.
+local classes = require "classes"
+-- Make sure to import the Superclass!
+local alert = require "alert"
+local json = require "dkjson"
+-- Import Mitre Att&ck utils
+local mitre = require "mitre_utils"
+
+-- ##############################################
+
+local flow_alert_host_policy = classes.class(alert)
+
+-- ##############################################
+
+flow_alert_host_policy.meta = {
+   alert_key = flow_alert_keys.flow_alert_host_policy,
+   icon = "fas fa-fw fa-exclamation",
+   i18n_title = "alerts_dashboard.access_control_list",
+}
+
+-- ##############################################
+
+-- @brief Prepare an alert table used to generate the alert
+-- @return A table with the alert built
+function flow_alert_host_policy:init()
+   -- Call the parent constructor
+   self.super:init()
+end
+
+-- #######################################################
+
+-- @brief Format an alert into a human-readable string
+-- @param ifid The integer interface id of the generated alert
+-- @param alert The alert description table, including alert data such as the generating entity, timestamp, granularity, type
+-- @param alert_type_params Table `alert_type_params` as built in the `:init` method
+-- @return A human-readable string
+function flow_alert_host_policy.format(ifid, alert, alert_type_params)
+   -- Extracting info field
+   local href = ntop.getHttpPrefix() .. '/lua/admin/network_configuration.lua?page=policy'
+   return(i18n("alerts_dashboard.access_control_list_descr", { href = href }))
+end
+
+-- #######################################################
+
+return flow_alert_host_policy
